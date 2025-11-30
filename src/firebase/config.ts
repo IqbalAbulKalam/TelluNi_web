f5c36b9ffbf73e96fb88004610ae2d6b59f3dbe3
Abul Kalam Iqbal <muhiqbalabulkalam@gmail.com>
2025-11-30 15:06:46 +0000

    batalkan penggunaan firebase, saya akan menggunakan opsi hosting gambar

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-9428449838-7ab34",
-  "appId": "1:1064538335964:web:924cb8bf01d2299d80a219",
-  "apiKey": "AIzaSyCiwmW_LyebQdVYbZSf-huCH78s5tKWv84",
-  "authDomain": "studio-9428449838-7ab34.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1064538335964"
-};
