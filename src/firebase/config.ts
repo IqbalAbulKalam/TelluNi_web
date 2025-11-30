3efcac50acc07e6f3f0c7f68ddd5c2fd72c8e35b
Abul Kalam Iqbal <muhiqbalabulkalam@gmail.com>
2025-11-30 15:05:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9428449838-7ab34",
+  "appId": "1:1064538335964:web:924cb8bf01d2299d80a219",
+  "apiKey": "AIzaSyCiwmW_LyebQdVYbZSf-huCH78s5tKWv84",
+  "authDomain": "studio-9428449838-7ab34.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1064538335964"
+};
